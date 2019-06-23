b75ebff72d59a44ea7849c4b6a369770893d2a1a
Suresh Tudor <suresh.tudor@gmail.com>
2019-06-23 09:40:41 -0700

    create routes file

--- server.js
+++ server.js
@@ -19,27 +19,8 @@ app.use(bodyParser.json())
 // import mock data
 const mock = require('./mock-data')
 
-// routes
-app.route('/api/applications').get((req, res) => {
-    console.log('request: '+ req.originalUrl);
-    res.send(mock.applications());
-});
-
-app.route('/api/applications/:id').get((req, res) => {
-    console.log('request: '+ req.originalUrl);
-    const filteredArray = mock.applications().filter(e => {
-        return e.id === Number(req.params['id']);
-    });
-    res.send(filteredArray);
-});
-
-app.route('/api/app-search').get((req, res) => {
-    console.log('request: '+ req.originalUrl);
-    const filteredArray = mock.applications().filter(e => {
-        return e.invitationId === Number(req.query.invitationId);
-    });
-    res.send(filteredArray);
-});
+// routes for application search
+require('./routes/search')(app, mock);
 
 // start server
 app.listen(3000, () => {
